18f87dae929c45447ea97ef929c82cb3977dfbc7
Darmin <254663604@qq.com>
2022-11-10 15:11:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
     response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
-    response.end(JSON.stringify({a: 1, b:{c: 2}}));
+    response.end(JSON.stringify({
+      "isBase64Encoded": false,
+      "statusCode": 200,
+      "headers": {"Content-Type":"text/html"},
+      "body": "<html><body><h1>Heading</h1><p>Paragraph.</p></body></html>"
+    }));
 };
 var www = http.createServer(handleRequest);
 www.listen(9000);
