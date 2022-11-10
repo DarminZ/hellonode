a9d990bdd6982d60dcc1085a7e86730685dc4656
Darmin <254663604@qq.com>
2022-11-10 14:53:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
     response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
-    response.send({a: 1, b:{c: 2}});
+    response.end(JSON.stringify({a: 1, b:{c: 2}}));
 };
 var www = http.createServer(handleRequest);
 www.listen(9000);
